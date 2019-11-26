a4578a0d484490681c8c2a2d16e632aef8d3e96a
Reko Meriö <K9260@student.jamk.fi>
2019-11-26 16:29:29 +0200

    Search field - Reko & Markus

---
+++ src/components/SearchField.js
@@ -0,0 +1,66 @@
+import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+
+import { fade, makeStyles } from "@material-ui/core/styles";
+
+export default function Sidebar(props) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.search}>
+            <div className={classes.searchIcon}>
+                <SearchIcon />
+            </div>
+            <InputBase
+                placeholder="Search…"
+                onChange={props.onSearch}
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput
+                }}
+                inputProps={{ "aria-label": "search" }}
+            />
+        </div>
+    );
+}
+
+const useStyles = makeStyles(theme => ({
+    grow: {
+        flexGrow: 1
+    },
+    search: {
+        position: "relative",
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        "&:hover": {
+            backgroundColor: fade(theme.palette.common.white, 0.25)
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: "100%",
+        [theme.breakpoints.up("sm")]: {
+            width: "auto"
+        }
+    },
+    searchIcon: {
+        width: theme.spacing(7),
+        height: "100%",
+        position: "absolute",
+        pointerEvents: "none",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    inputRoot: {
+        color: "inherit"
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("md")]: {
+            width: 200
+        }
+    }
+}));
