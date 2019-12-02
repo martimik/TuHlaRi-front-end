b409cbce964c1a9957904e2e348e9bb297946d6a
Reko Meriö <K9260@student.jamk.fi>
2019-12-02 15:08:23 +0200

    Routing changes - Reko & Markus

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import axios from "axios";
-import logo from "../logo.svg";
 import Nav from "./Nav";
 import Products from "./Products";
+import CreateProduct from "./CreateProduct";
 
 function App() {
-    const array = [
+    const links = [
         { name: "Home", url: "/" },
-        { name: "Products", url: "products" }
+        { name: "Products", url: "products" },
+        { name: "Create product", url: "create-product" }
     ];
 
     return (
         <div className="App">
             <Router>
-                <Nav links={array} />
+                <Nav links={links} />
                 <div>
                     <Switch>
+                        <Route exact path="/">
+                            <div>
+                                <p>Home</p>
+                            </div>
+                        </Route>
                         <Route path="/products">
                             <Products />
+                        </Route>
+                        <Route path="/create-product">
+                            <CreateProduct />
                         </Route>
                     </Switch>
                 </div>
