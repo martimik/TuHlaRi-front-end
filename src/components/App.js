9389ac35515e5971395f9ab27e9319545b19053f
opiskelija <opiskelija@IT-L-PC0JE17R.it.local>
2020-01-20 14:47:00 +0200

    Merge finished

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,6 @@ import { UserProvider } from "./UserContext";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "../theme";
 import DeletedProducts from "./DeletedProducts";
-import CreateUser from "./CreateUser";
 import ProductsView from "./ProductsView";
 import API_URL from "../js/api";
 import ProductView from "./ProductView";
@@ -20,34 +19,18 @@ import ProductView from "./ProductView";
 axios.defaults.withCredentials = true;
 
 function App() {
-  const [authorization, setAuthorization] = useState({
-    email: "",
-    userGroup: null,
-    name: ""
-  });
+    const [authorization, setAuthorization] = useState({
+        email: "",
+        userGroup: null,
+        name: ""
+    });
 
-  const USERGROUP = {
-    ADMIN: "0",
-    PRODUCT_OWNER: "1",
-    SALESPERSON: "2"
-  };
+    const USERGROUP = {
+        ADMIN: "0",
+        PRODUCT_OWNER: "1",
+        SALESPERSON: "2"
+    };
 
-<<<<<<< src/components/App.js
-  const links = [
-    { name: "Home", url: "/" },
-    { name: "Products", url: "products" }
-  ];
-
-  if (authorization.userGroup) {
-    links.push({ name: "Create product", url: "create-product" });
-  }
-
-  if (authorization.userGroup === USERGROUP.ADMIN) {
-    links.push({ name: "Users", url: "users" });
-    links.push({ name: "Create user", url: "create-user" });
-    links.push({ name: "Deleted products", url: "deleted-products" });
-  }
-=======
     const links = [
         { name: "Home", url: "/" },
         { name: "Products", url: "/products" }
@@ -58,79 +41,30 @@ function App() {
     }
 
     if (authorization.userGroup === USERGROUP.ADMIN) {
+        links.push({ name: "Users", url: "/users" });
         links.push({ name: "Create user", url: "/create-user" });
+        links.push({ name: "Deleted products", url: "/deleted-products" });
     }
->>>>>>> src/components/App.js
 
-  useEffect(() => {
-    getLoginState();
-    setInterval(getLoginState, 1000 * 60); // Read login state every minute
-  }, []);
+    useEffect(() => {
+        getLoginState();
+        setInterval(getLoginState, 1000 * 60); // Read login state every minute
+    }, []);
 
-  function getLoginState() {
-    axios
-      .get(API_URL + "session")
-      .then(response => {
-        const { email, name, userGroup } = response.data;
-        if (email && userGroup) {
-          setAuthorization({ email, name, userGroup });
-        }
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
+    function getLoginState() {
+        axios
+            .get(API_URL + "session")
+            .then(response => {
+                const { email, name, userGroup } = response.data;
+                if (email && userGroup) {
+                    setAuthorization({ email, name, userGroup });
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
 
-<<<<<<< src/components/App.js
-  return (
-    <div className="App">
-      <ThemeProvider theme={theme}>
-        <UserProvider value={authorization}>
-          <SnackbarProvider preventDuplicate maxSnack={3}>
-            <Router>
-              <Nav
-                links={links}
-                authorization={authorization}
-                setAuthorization={setAuthorization}
-              />
-              <div>
-                <Switch>
-                  <Route exact path="/">
-                    <div>
-                      <p>Home</p>
-                    </div>
-                  </Route>
-                  <Route path="/products">
-                    <Products />
-                  </Route>
-                  <Route path="/create-product">
-                    <CreateProduct />
-                  </Route>
-                  <Route path="/settings">
-                    <Settings />
-                  </Route>
-                  <Route path="/users">
-                    <Users />
-                  </Route>
-                  <Route path="/create-user">
-                    {authorization.userGroup === USERGROUP.ADMIN ? (
-                      <CreateUser />
-                    ) : (
-                      <div>Not found</div>
-                    )}
-                  </Route>
-                  <Route path="/deleted-products">
-                    <DeletedProducts />
-                  </Route>
-                </Switch>
-              </div>
-            </Router>
-          </SnackbarProvider>
-        </UserProvider>
-      </ThemeProvider>
-    </div>
-  );
-=======
     return (
         <div className="App">
             <ThemeProvider theme={theme}>
@@ -159,6 +93,15 @@ function App() {
                                     <Route path="/product/:id">
                                         <ProductView />
                                     </Route>
+                                    <Route path="/settings">
+                                        <Settings />
+                                    </Route>
+                                    <Route path="/users">
+                                        <Users />
+                                    </Route>
+                                    <Route path="/deleted-products">
+                                        <DeletedProducts />
+                                    </Route>
                                     <Route path="/create-product">
                                         {authorization.userGroup ? (
                                             <CreateProduct />
@@ -182,7 +125,6 @@ function App() {
             </ThemeProvider>
         </div>
     );
->>>>>>> src/components/App.js
 }
 
 export default App;
