dd2a83269f9429f81f34e01222a70cbfec7ff2f0
opiskelija <opiskelija@IT-L-PC0JE17R.it.local>
2020-01-21 11:01:57 +0200

    Build

---
+++ build/precache-manifest.6288930da9f79dea389a9ab85ebb7841.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d069180a2a05f07dd813c450cfd58a9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee4a0881a8b22a4db2d9",
+    "url": "./static/css/main.a4cf12c3.chunk.css"
+  },
+  {
+    "revision": "b7bb049be06c814c9594",
+    "url": "./static/js/2.0f8970f8.chunk.js"
+  },
+  {
+    "revision": "ee4a0881a8b22a4db2d9",
+    "url": "./static/js/main.160a85fb.chunk.js"
+  },
+  {
+    "revision": "78f1545793679ee67167",
+    "url": "./static/js/runtime-main.ed055766.js"
+  }
+]);
