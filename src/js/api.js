33384dc501e9196f0026f85312554b745262e1c5
K9260 <k9260@student.jamk.fi>
2020-01-22 09:56:31 +0200

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,5 @@
-const API_URL = window.location.host.includes("localhost")
-    ? "http://localhost:8080/"
-    : "http://nodejs-mongo-persistent-tuhlari.rahtiapp.fi/";
+const API_URL = process.env.API_URL
+    ? process.env.API_URL
+    : "http://localhost:8080/";
 
 export default API_URL;
