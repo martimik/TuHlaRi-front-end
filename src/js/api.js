b753b1644a6c0c16b9fcc2f24f5172b5b101353d
K9260 <k9260@student.jamk.fi>
2020-01-22 10:12:38 +0200

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,5 @@
-const API_URL = process.env.API_URL
-    ? process.env.API_URL
+const API_URL = process.env.REACT_APP_API_URL
+    ? process.env.REACT_APP_API_URL
     : "http://localhost:8080/";
 
 export default API_URL;
