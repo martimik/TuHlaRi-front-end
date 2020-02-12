66f20e7e8f7a1818fe84e64a11cf8ddd2bdf94c0
opiskelija <opiskelija@IT-L-PC0JE17R.it.local>
2020-02-12 10:20:51 +0200

    Add App.js to tests

---
+++ src/components/CreateProduct.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import ProductEditor from "./ProductEditor";
+
+export default function CreateProduct() {
+    return (
+        <div>
+            <ProductEditor title="Create new product" />
+        </div>
+    );
+}
